c0c2614b447a54ce2e0a2f0f3509faa106197c41
Isamu Ito <isamsito@gmail.com>
2021-04-01 16:50:54 +0000

    Require authentication before access admin pages

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+
+  before_filter :authorize
+  
   def show
     @products_count = Product.count()
     @categories_count = Category.count()
