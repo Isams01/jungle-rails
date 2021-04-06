71b0cbd0a93dc7022b54bf18ab7e74f54f4d1374
Isamu Ito <isamsito@gmail.com>
2021-04-06 05:28:35 +0000

    Return user object

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   def self.authenticate_with_credentials(email, password)
     @user = User.find_by_email(email)
     if @user && @user.authenticate(password)
+      @user
     else
       nil
     end
