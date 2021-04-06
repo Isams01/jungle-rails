883d668fc2ac42b6431b5c218707514dab5f7c59
Isamu Ito <isamsito@gmail.com>
2021-04-06 04:17:45 +0000

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :first_name, presence: true, length: {minimum: 3}
+  validates :last_name, presence: true, length: {minimum: 3}
+  validates :email, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length {minimum: 5}
 end
